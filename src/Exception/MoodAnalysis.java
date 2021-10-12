755ff094beb47cae8b5fa383997737a942c1e945
Priyankadoke12345 <priyankaadoke@gmail.com>
2021-10-12 10:13:51 +0530

    priyanka custom

--- src/Exception/MoodAnalysis.java
+++ src/Exception/MoodAnalysis.java
@@ -3,6 +3,8 @@ package Exception;
 public class MoodAnalysis {
     public String analysisMood(String mood)
     {
-
+        if(mood.equals("I am in sad Mood")){
+            return "SAD";
     }
+
 }
